7efcafcb3a95184a1e62bd8b9cbc5400e5a15d8c
Eliza <ekoknapp@gmail.com>
2021-05-17 21:07:43 -0400

    changed nothing in sockets, switched link in frontend

--- client/src/ClientSocket.js
+++ client/src/ClientSocket.js
@@ -1,7 +1,7 @@
 import socketIOClient from "socket.io-client";
 
 // const endpoint = window.location.hostname + ":" + 65080;
-const socket = socketIOClient("https://websocket-dot-quarcade.uk.r.appspot.com", {
+const socket = socketIOClient("https://socket-dot-quarcade.uk.r.appspot.com/socket.io/?EIO=4&transport=polling&t=Nbz0VfM", {
   withCredentials: true
 });
 
